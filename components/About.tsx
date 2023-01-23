1655f8f530516216e9ac6fbaa8742e517f6fc0f1
Timothy Williams <williamstim146@gmail.com>
2023-01-23 17:46:02 -0600

    fix skill section and adjust a bit of About section for better display on mobile.

--- components/About.tsx
+++ components/About.tsx
@@ -25,7 +25,7 @@ function About({}: Props) {
         }}
         src="https://media.licdn.com/dms/image/C4D03AQEp_4CsHGLEmg/profile-displayphoto-shrink_800_800/0/1552595666470?e=1678924800&v=beta&t=c8w_laBdlhi5hmJQFxRT5GgzEThG1mvdskfMKSOM5ZI"
         alt=''
-        className='-mb-20 md:mb-0 flex-shrink-0 w-36 h-36 rounded-full object-cover md:rounded-lg md:w-[300px] md:h-[300px] xl:w-[400px] xl:h-[400px] mt-[75px]'
+        className='-mb-20 md:mb-0 flex-shrink-0 w-32 h-32 rounded-full object-cover md:rounded-lg md:w-[300px] md:h-[300px] xl:w-[400px] xl:h-[400px] mt-[75px]'
       />
       <motion.div
         initial={{
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -21,11 +21,11 @@ function Skill({ directionLeft, url }: Props) {
         }}
         src={`${url}`}
         alt=''
-        className='rounded-full border border-gray-500 object-contain h-[75px] w-[75px] filter group-hover:grayscale transition duration-300 ease-in-out'
+        className='rounded-full border border-gray-500 object-contain h-16 md:h-24 w-16 md:w-24 filter group-hover:grayscale transition duration-300 ease-in-out'
       />
-      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-[75px] w-[75px] rounded-full z-0'>
+      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-16 md:h-24 w-16 md:w-24 rounded-full z-0'>
         <div className='flex items-center justify-center h-full'>
-          <p className='text-2xl font-bold text-black opacity-100'>100%</p>
+          <p className='text-lg md:text-2xl font-bold text-black opacity-100'>100%</p>
         </div>
       </div>
     </div>
