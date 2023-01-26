d6cccb6c7ba1e17ac8e681498bbf1442e9c29965
Timothy Williams <williamstim146@gmail.com>
2023-01-25 20:31:11 -0600

    Set up full project loading in Sanity resources and styled the way it should be. Now, I just need to upload to correct pictures, and deploy!

--- App/components/Projects.tsx
+++ App/components/Projects.tsx
@@ -9,12 +9,16 @@ type Props = {
   projects: Project[];
 }
 
+type align = {
+  align: string
+}
+
 function Projects({projects}: Props) {
   return (
     <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
 
-      <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track--gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
+      <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track--gray-400/20 scrollbar-thumb-[#F7AB0A]/80 mt-10'>
         {projects.map((project, i) => (
           <motion.div
             key={project?._id}
@@ -27,7 +31,7 @@ function Projects({projects}: Props) {
             transition={{
               duration: 1.5,
             }}
-            className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-10 pt-20 md:pt-30 md:p-20 h-screen'>
+            className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-10 pt-25 md:p-20 md:pt-45 h-screen'>
             <motion.img
               initial={{
                 y: -300,
@@ -48,7 +52,7 @@ function Projects({projects}: Props) {
               className='h-36 w-36'
             />
 
-            <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
+            <div className='space-y-5 px-0 md:px-10 max-w-6xl justify-evenly items-center'>
               <h4 className='text-2xl md:text-4xl font-semibold text-center'>
                 <span className='underline decoration=[#F7AB0A]/50'>
                   Project {i + 1} of {projects.length}:
@@ -56,20 +60,20 @@ function Projects({projects}: Props) {
                 {project?.title}
               </h4>
 
-              <div className='flex items-center space-x-2 justify-center'>
+              <p className='w-4/5 mx-auto' align="center">
+                {project?.summary}
+              </p>
+
+                <div className='grid grid-cols-5 md:flex md:flex-row items-center justify-center mx-auto'>
                 {project?.technologies.map((technology) => {
                   return <img
                     key={project?._id}
                     src={urlFor(technology.image).url()}
                     alt=''
-                    className='h-10 w-10'
+                    className='h-8 w-8 m-1 md:m-2'
                   />
                 })}
               </div>
-
-              <p>
-                {project?.title}
-              </p>
               </div>
           </motion.div>
         ))}
--- App/components/Skills.tsx
+++ App/components/Skills.tsx
@@ -24,7 +24,7 @@ function Skills({skills}: Props) {
 
       <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over a skill for current proficiency</h3>
 
-      <div className='grid grid-cols-3 gap-5 pt-[150px] md:grid-cols-4'>
+      <div className='grid grid-cols-4 gap-5 pt-[150px] md:grid-cols-5'>
         {skills.map((skill) => {
           return <Skill
             key={skill._id}
