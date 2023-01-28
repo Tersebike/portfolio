2c01c8bdfe449f186d3371bda4018422fb9e1b20
Timothy Williams <williamstim146@gmail.com>
2023-01-28 13:57:15 -0600

    Change styling on contact page to show better on desktop. Will update deployment.

--- App/components/ContactMe.tsx
+++ App/components/ContactMe.tsx
@@ -19,7 +19,7 @@ function ContactMe({}: Props) {
   return (
     <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
       <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl pl-[20px]'>Contact</h3>
-      <div className='flex flex-col space-y-2 mb-10'>
+      <div className='flex flex-col space-y-2 mb-10 md:mb-0 md:mt-16'>
         <h4 className='text-2xl font-semibold text-center'>
           I have got just what you need.{' '}
           <span className='decoration-[#F7AB0A]/50 underline'>Lets Talk.</span>
